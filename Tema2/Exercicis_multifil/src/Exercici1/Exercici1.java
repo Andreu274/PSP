3f21b93656d4cc6b99eb593a163db76007dacacc
Andreu274 <andreupons@paucasesnovescifp.cat>
2023-10-31 18:57:24 +0100

    Exercici1

---
+++ Tema2/Exercicis_multifil/src/Exercici1/Exercici1.java
@@ -0,0 +1,13 @@
+package Exercici1;
+
+public class Exercici1 {
+    public static void main(String[] args) {
+        int N = 5; // Número de hilos a crear
+
+        for (int i = 1; i <= N; i++) {
+            Thread hilo = new Thread(new MiHilo(i));
+            hilo.start();
+        }
+    }
+}
+
